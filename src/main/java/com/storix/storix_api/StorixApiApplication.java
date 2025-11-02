be5fd33fb1446e1766b7e45de9c515b8e95af677
caminobelllo <alstpgml0812@gmail.com>
2025-11-02 23:05:39 +0900

    Chore: spring boot initial

---
+++ src/main/java/com/storix/storix_api/StorixApiApplication.java
@@ -0,0 +1,13 @@
+package com.storix.storix_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorixApiApplication.class, args);
+	}
+
+}
