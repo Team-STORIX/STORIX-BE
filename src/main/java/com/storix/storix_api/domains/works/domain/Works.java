a87c82e406a52d2664bbefc520b56fd930baab19
caminobelllo <alstpgml0812@gmail.com>
2025-11-15 03:00:16 +0900

    Feat: 매핑관계 및 빌더 추가

--- src/main/java/com/storix/storix_api/domains/works/domain/Works.java
+++ src/main/java/com/storix/storix_api/domains/works/domain/Works.java
@@ -1,11 +1,16 @@
 package com.storix.storix_api.domains.works.domain;
 
+import com.storix.storix_api.domains.hashtag.domain.Hashtag;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "works")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Works {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,14 +47,14 @@ public class Works {
 
     // 작품 소개
     @Lob
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
     // 작품 장르
     @Column(nullable = false)
     private Genre genre;
 
-    @Column(name = "thumbnail_url", nullable = false)
+    @Column(name = "thumbnail_url", nullable = false, length = 500)
     private String thumbnailUrl;
 
     @Column(name = "reviews_count")
@@ -60,4 +65,44 @@ public class Works {
 
     @Column(name = "works_type", nullable = false)
     private WorksType worksType;
+
+    @ToString.Exclude
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "works_hashtag",
+            joinColumns = @JoinColumn(name = "works_id"),
+            inverseJoinColumns = @JoinColumn(name = "hashtag_id")
+    )
+    private Set<Hashtag> hashtags = new HashSet<>();
+
+    @Builder
+    private Works(Long userId, Platform platform, String worksName,
+                  String artistName, String author, String illustrator,
+                  String originalAuthor, AgeClassification ageClassification,
+                  String description, Genre genre, String thumbnailUrl,
+                  WorksType worksType) {
+
+        this.userId = userId;
+        this.platform = platform;
+        this.worksName = worksName;
+        this.artistName = artistName;
+        this.author = author;
+        this.illustrator = illustrator;
+        this.originalAuthor = originalAuthor;
+        this.ageClassification = ageClassification;
+        this.description = description;
+        this.genre = genre;
+        this.thumbnailUrl = thumbnailUrl;
+        this.worksType = worksType;
+        this.reviewsCount = 0;
+        this.avgRating = 0.0f;
+        this.hashtags = new HashSet<>();
+    }
+
+    /**
+     * 해시태그 조회 (read-only)
+     * */
+    public Set<Hashtag> getHashtags() {
+        return Set.copyOf(hashtags);
+    }
 }
