591a61d4f58578c3374fb45f68299f34d0541188
Immmii <leesa7118@gmail.com>
2025-11-15 04:49:18 +0900

    Style: User domain FavoriteGenre.java 포맷 변경

--- src/main/java/com/storix/storix_api/domains/user/domain/FavoriteGenre.java
+++ src/main/java/com/storix/storix_api/domains/user/domain/FavoriteGenre.java
@@ -1,5 +1,18 @@
 package com.storix.storix_api.domains.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum FavoriteGenre {
-    ROMANCE, THRILLER, FANTASY, DAILY, ROMANCE_FANTASY, COMEDY, DRAMA
+    ROMANCE("로맨스"),
+    THRILLER("스릴러"),
+    FANTASY("판타지"),
+    DAILY("일상"),
+    ROMANCE_FANTASY("로맨스 판타지"),
+    COMEDY("코미디"),
+    DRAMA("드라마");
+
+    private String value;
 }
