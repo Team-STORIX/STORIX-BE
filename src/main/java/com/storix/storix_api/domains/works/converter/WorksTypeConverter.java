f1d63a060d742281dda1a65b98f414994b9c49c9
caminobelllo <alstpgml0812@gmail.com>
2025-11-15 03:00:01 +0900

    Feat: WorksTypeConverter 구현

--- src/main/java/com/storix/storix_api/domains/works/converter/WorksTypeConverter.java
+++ src/main/java/com/storix/storix_api/domains/works/converter/WorksTypeConverter.java
@@ -4,16 +4,27 @@ import com.storix.storix_api.domains.works.domain.WorksType;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
+import java.util.stream.Stream;
+
 @Converter(autoApply = true)
 public class WorksTypeConverter implements AttributeConverter<WorksType, String> {
 
     @Override
     public String convertToDatabaseColumn(WorksType attribute) {
-        return "";
+
+        if  (attribute == null) { return null; }
+
+        return attribute.getDbValue();
     }
 
     @Override
     public WorksType convertToEntityAttribute(String dbData) {
-        return null;
+
+        if (dbData == null || dbData.isEmpty()) { return null; }
+
+        return Stream.of(WorksType.values())
+                .filter(c -> c.getDbValue().equals(dbData))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown DB value: " + dbData));
     }
 }
