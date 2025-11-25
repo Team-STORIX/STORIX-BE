75e28b2a70f317a842fe11e712414c6d75faecf5
Immmii <leesa7118@gmail.com>
2025-11-25 14:38:02 +0900

    Refactor: userId 반환타입 변경

--- src/main/java/com/storix/storix_api/global/security/dto/AccessTokenInfo.java
+++ src/main/java/com/storix/storix_api/global/security/dto/AccessTokenInfo.java
@@ -3,10 +3,10 @@ package com.storix.storix_api.global.security.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
 @Builder
-public class AccessTokenInfo {
-    private final Long userId;
-    private final String role;
+public record AccessTokenInfo (
+    Long userId,
+    String role
+) {
 }
 
