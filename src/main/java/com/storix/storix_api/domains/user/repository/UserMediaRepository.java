9fbc71ef05cd790a240513c8f30ec6b07114569f
Immmii <leesa7118@gmail.com>
2025-11-25 20:20:36 +0900

    Remove: 미구현 EntityRepository 삭제

--- src/main/java/com/storix/storix_api/domains/user/repository/UserMediaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.storix.storix_api.domains.user.repository;
-
-import com.storix.storix_api.domains.user.domain.UserMedia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserMediaRepository extends JpaRepository<UserMedia, Long> {
-}
