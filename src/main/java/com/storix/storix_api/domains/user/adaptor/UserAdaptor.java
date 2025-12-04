5455fea2c1e6da274b59fc91b82a6c1c447e6fe1
Immmii <leesa7118@gmail.com>
2025-12-04 23:18:40 +0900

    Feat: user adaptor 독자 계정 조회 및 회원 가입 로직 추가

--- src/main/java/com/storix/storix_api/domains/user/adaptor/UserAdaptor.java
+++ src/main/java/com/storix/storix_api/domains/user/adaptor/UserAdaptor.java
@@ -1,12 +1,15 @@
 package com.storix.storix_api.domains.user.adaptor;
 
+import com.storix.storix_api.domains.user.domain.OAuthInfo;
+import com.storix.storix_api.domains.user.domain.OAuthProvider;
 import com.storix.storix_api.domains.user.domain.User;
+import com.storix.storix_api.domains.user.dto.CreateReaderUserCommand;
 import com.storix.storix_api.domains.user.dto.LoginInfo;
 import com.storix.storix_api.domains.user.repository.UserRepository;
 import com.storix.storix_api.domains.user.dto.CreateArtistUserCommand;
 import com.storix.storix_api.global.apiPayload.code.ErrorCode;
-import com.storix.storix_api.global.apiPayload.exception.ArtistLoginException;
-import com.storix.storix_api.global.apiPayload.exception.UnknownUserException;
+import com.storix.storix_api.global.apiPayload.exception.user.ArtistLoginException;
+import com.storix.storix_api.global.apiPayload.exception.user.UnknownUserException;
 import com.storix.storix_api.global.apiPayload.exception.ErrorResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +26,38 @@ public class UserAdaptor {
 
     // TODO: 인덱싱
 
-    // 작가
+    /**
+     * 독자
+     * */
+    // 독자
+    // OAuthInfo(oid, provider) -> userId(PK), role (토큰 서명용)
+    public AuthUserDetails findReaderUserByOAuthInfo(OAuthInfo oauthInfo) {
+        Optional<User> readerUser = userRepository.findByOauthInfoProviderAndOauthInfoOid(oauthInfo.getProvider(), oauthInfo.getOid());
+        if (readerUser.isPresent()) {
+            return new AuthUserDetails(readerUser.get().getId(), readerUser.get().getRole().toString());
+        } else {
+            throw UnknownUserException.EXCEPTION;
+        }
+    }
+
+    public boolean isUserPresentWithProviderAndOid(OAuthProvider provider, String oid) {
+        Optional<User> readerUser = userRepository.findByOauthInfoProviderAndOauthInfoOid(provider, oid);
+        if (readerUser.isPresent()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 독자 회원 가입
+    public AuthUserDetails saveReaderUser(CreateReaderUserCommand cmd) {
+        User user = userRepository.save(cmd.toEntity());
+        return new AuthUserDetails(user.getId(), user.getRole().toString());
+    }
+
+    /**
+     * 작가
+     * */
     // loginId -> userId (회원가입 api 응답, 작품-작가 매칭 용)
     public Long findArtistUserIdByLoginId(String loginId){
         Optional<User> artistUser = userRepository.findArtistUserByLoginId(loginId);
@@ -62,8 +96,5 @@ public class UserAdaptor {
         return null;
     }
 
-    public User saveArtistUser(CreateArtistUserCommand cmd) {
-        User user = userRepository.save(cmd.toEntity());
-        return userRepository.save(user);
-    }
+    public void saveArtistUser(CreateArtistUserCommand cmd) { userRepository.save(cmd.toEntity()); }
 }
