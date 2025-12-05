7593db7fd3f9af6f8653a1d5ddc5f7053997a178
Immmii <leesa7118@gmail.com>
2025-12-05 13:19:25 +0900

    Feat: refreshToken adaptor 로그아웃 로직 구현

--- src/main/java/com/storix/storix_api/domains/user/adaptor/RefreshTokenAdaptor.java
+++ src/main/java/com/storix/storix_api/domains/user/adaptor/RefreshTokenAdaptor.java
@@ -2,8 +2,11 @@ package com.storix.storix_api.domains.user.adaptor;
 
 import com.storix.storix_api.domains.user.domain.RefreshToken;
 import com.storix.storix_api.domains.user.repository.RefreshTokenRepository;
+import com.storix.storix_api.global.apiPayload.exception.user.InvalidTokenException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +18,16 @@ public class RefreshTokenAdaptor {
         return refreshTokenRepository.save(refreshToken);
     }
 
-    // logout 기능 구현용
-//    public RefreshToken findByRefreshToken(String refreshToken) {
-//        return refreshTokenRepository.findByRefreshToken(refreshToken)
-//                .orElseThrow(() -> new IllegalArgumentException("Invalid refresh token")); // 커스텀 에러로 변경
-//    }
-//
-//    public void deleteByUserId(Long userId) {
-//        refreshTokenRepository.deleteById(userId.toString());
-//    }
+    public Long findUserIdByRefreshToken(String refreshToken) {
+        Optional<RefreshToken> refreshTokenInfo = refreshTokenRepository.findByRefreshToken(refreshToken);
+        if (!refreshTokenInfo.isPresent()) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+
+        return refreshTokenInfo.get().getId();
+    }
+
+    public void deleteByUserId(Long userId) {
+        refreshTokenRepository.deleteById(userId);
+    }
 }
