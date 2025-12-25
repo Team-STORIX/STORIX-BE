ca26d3ce6ee75c11c15738d3d187539a4432e303
Immmii <leesa7118@gmail.com>
2025-12-25 23:44:11 +0900

    Feat: [FeignClient] 네이버 ErrorDecoder 구현

--- src/main/java/com/storix/storix_api/global/apiPayload/code/ErrorCode.java
+++ src/main/java/com/storix/storix_api/global/apiPayload/code/ErrorCode.java
@@ -43,6 +43,17 @@ public enum ErrorCode {
     KOE320(HttpStatus.BAD_REQUEST, "KAKAO_KOE320", "동일한 인가 코드를 두 번 이상 사용하거나, 이미 만료된 인가 코드를 사용한 경우, 혹은 인가 코드를 찾을 수 없는 경우입니다."),
     KOE_INVALID_REQUEST(HttpStatus.BAD_REQUEST, "KAKAO_KOE_INVALID_REQUEST","잘못된 요청인 경우"),
 
+    // Naver OAuth error
+    NOE024(HttpStatus.UNAUTHORIZED, "NAVER_NOE024", "OAuth 인증에 실패했습니다."),
+    NOE028(HttpStatus.UNAUTHORIZED, "NAVER_NOE028", "OAuth 인증 헤더가 없습니다."),
+    NOE403(HttpStatus.FORBIDDEN, "NAVER_NOE403", "OAuth 호출 권한이 없습니다."),
+    NOE404(HttpStatus.NOT_FOUND, "NAVER_NOE404", "OAuth 검색 결과가 없습니다."),
+    NOE500(HttpStatus.BAD_REQUEST, "NAVER_NOE500", "네이버 데이터 베이스 오류입니다."),
+    NOE_INVALID_REQUEST(HttpStatus.BAD_REQUEST, "NAVER_NOE_INVALID_REQUEST", "파라미터가 잘못되었거나 요청문이 잘못되었습니다."),
+    NOE_UNAUTHORIZED_CLIENT(HttpStatus.BAD_REQUEST, "NAVER_NOE_UNAUTHORIZED_CIENT", "인증받지 않은 인증 코드로 요청했습니다."),
+    NOE_UNSUPPORTED_RESPONSE_TYPE(HttpStatus.BAD_REQUEST, "NAVER_NOE_UNSUPPORTED_RESPONSE_TYPE", "정의되지 않은 반환 형식으로 요청했습니다."),
+    NOE_SERVER_ERROR(HttpStatus.BAD_REQUEST, "NAVER_SERVER_ERROR", "네이버 인증 서버의 오류로 요청을 처리하지 못했습니다."),
+
     // OIDC error
     OIDC_OLD_PUBLIC_KEY_ERROR(HttpStatus.BAD_REQUEST, "OIDC_ERORR_1", "OIDC 공개키 갱신이 필요합니다."),
 
