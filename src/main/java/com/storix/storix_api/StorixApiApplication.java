72a78872b3ad84d3d8cacef9ebd90cd4a447ac2b
caminobelllo <alstpgml0812@gmail.com>
2025-12-26 17:24:33 +0900

    Refactor: 비동기 어노테이션 설정

--- src/main/java/com/storix/storix_api/StorixApiApplication.java
+++ src/main/java/com/storix/storix_api/StorixApiApplication.java
@@ -2,9 +2,9 @@ package com.storix.storix_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class StorixApiApplication {
 
