2004f545713162ae230758bb2add6ebd0ab0ba1a
Immmii <leesa7118@gmail.com>
2025-12-29 19:04:33 +0900

    Refactor: ReaderSocialLoginResponse null 필드 미반환

--- src/main/java/com/storix/storix_api/controller/auth/dto/ReaderSocialLoginResponse.java
+++ src/main/java/com/storix/storix_api/controller/auth/dto/ReaderSocialLoginResponse.java
@@ -1,5 +1,8 @@
 package com.storix.storix_api.controller.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ReaderSocialLoginResponse(
         boolean isRegistered,
         ReaderLoginResponse readerLoginResponse,
