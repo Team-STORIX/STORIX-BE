f515c9e7bd460481e9b5f81928d255790b2888d2
Immmii <leesa7118@gmail.com>
2026-01-03 03:22:26 +0900

    Refactor: [User] lastLoginAt 필드 활성화

--- src/main/java/com/storix/storix_api/domains/user/application/service/ReaderLoginService.java
+++ src/main/java/com/storix/storix_api/domains/user/application/service/ReaderLoginService.java
@@ -5,6 +5,7 @@ import com.storix.storix_api.domains.user.application.usecase.helper.OAuthHelper
 import com.storix.storix_api.domains.user.adaptor.UserAdaptor;
 import com.storix.storix_api.domains.user.domain.OAuthInfo;
 import com.storix.storix_api.domains.user.domain.OAuthProvider;
+import com.storix.storix_api.domains.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,10 @@ public class ReaderLoginService {
     // 회원가입한 경우 로그인 처리
     public AuthUserDetails execute(String idToken, OAuthProvider provider) {
         OAuthInfo oauthInfo = oauthHelper.getOauthInfoByIdToken(idToken, provider);
-        return userAdaptor.findReaderUserByOAuthInfo(oauthInfo);
+
+        User user = userAdaptor.findReaderUserByOAuthInfo(oauthInfo);
+        user.login();
+        return new AuthUserDetails(user.getId(), String.valueOf(user.getRole()));
     }
 
     // 회원가입하지 않은 경우
