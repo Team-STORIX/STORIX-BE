32f18eb4946899d4eb3ebfb04262f2b2a70af705
Immmii <leesa7118@gmail.com>
2026-01-03 03:20:00 +0900

    Fix: [CreateReaderUserCommand] DTO 필드 수정

--- src/main/java/com/storix/storix_api/domains/user/dto/CreateReaderUserCommand.java
+++ src/main/java/com/storix/storix_api/domains/user/dto/CreateReaderUserCommand.java
@@ -23,7 +23,7 @@ public record CreateReaderUserCommand(
                 oauthInfo,
                 nickName,
                 gender,
-                favoriteGenreList
+                genres
         );
     }
 }
