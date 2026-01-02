c251ee7ed694645a57b1ef7f798e3e94e2faae1d
caminobelllo <alstpgml0812@gmail.com>
2026-01-02 22:02:00 +0900

    Refactor: 작가 검색 기능 분리

--- src/main/java/com/storix/storix_api/domains/works/repository/WorksRepository.java
+++ src/main/java/com/storix/storix_api/domains/works/repository/WorksRepository.java
@@ -11,10 +11,5 @@ import java.util.List;
 
 public interface WorksRepository extends JpaRepository<Works, Long> {
 
-    // 1. 작품 검색 (작품명, 작가명 포함)
-    Slice<Works> findByWorksNameContainingOrArtistNameContaining(String worksName, String artistName, Pageable pageable);
-
-    // 2. 작가명 검색 (String)
-    @Query("SELECT DISTINCT w.artistName FROM Works w WHERE w.artistName LIKE %:keyword%")
-    List<String> findDistinctArtistNames(@Param("keyword") String keyword, Pageable pageable);
+    Slice<Works> findByWorksNameContaining(String keyword, Pageable pageable);
 }
