fe108bc56bb6473062b0bc4f2b78947aa0581212
Immmii <leesa7118@gmail.com>
2026-01-04 03:39:28 +0900

    Feat: [global] required query param 누락 Exception Handler 구현

--- src/main/java/com/storix/storix_api/global/apiPayload/exception/FieldErrorResponse.java
+++ src/main/java/com/storix/storix_api/global/apiPayload/exception/FieldErrorResponse.java
@@ -1,5 +1,8 @@
 package com.storix.storix_api.global.apiPayload.exception;
 
+import lombok.Builder;
+
+@Builder
 public record FieldErrorResponse(
         String field,
         Object rejectedValue,
