cc568985ee89565bbdb4b874e150b3ead7b7db89
Immmii <leesa7118@gmail.com>
2026-01-06 07:50:49 +0900

    Refactor: 기본 프로필 조회 api 반환 필드 추가 및 변경: role, level

--- src/main/java/com/storix/storix_api/domains/profile/application/service/ProfileService.java
+++ src/main/java/com/storix/storix_api/domains/profile/application/service/ProfileService.java
@@ -17,6 +17,7 @@ public class ProfileService {
         User readerUser = userAdaptor.findUserById(userId);
 
         return UserInfo.builder()
+                .role(readerUser.getRole().getValue())
                 .nickName(readerUser.getNickName())
                 .level(readerUser.getLevel())
                 .profileDescription(readerUser.getProfileDescription())
@@ -29,6 +30,7 @@ public class ProfileService {
         User artistUser = userAdaptor.findUserById(userId);
 
         return UserInfo.builder()
+                .role(artistUser.getRole().getValue())
                 .nickName(artistUser.getNickName())
                 .profileDescription(artistUser.getProfileDescription())
                 .profileImageUrl(artistUser.getProfileImageUrl())
--- src/main/java/com/storix/storix_api/domains/user/domain/Role.java
+++ src/main/java/com/storix/storix_api/domains/user/domain/Role.java
@@ -1,5 +1,13 @@
 package com.storix.storix_api.domains.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Role {
-    READER, ARTIST
+    READER("독자"),
+    ARTIST("작가");
+
+    private final String value;
 }
