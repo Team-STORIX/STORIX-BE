6ff5dcf699c1fa43792fe1bb24e7ecbc98c288e5
caminobelllo <alstpgml0812@gmail.com>
2026-01-06 22:12:23 +0900

    Feat: 토픽룸 도메인 설정

---
+++ src/main/java/com/storix/storix_api/domains/topicroom/domain/TopicRoom.java
@@ -0,0 +1,42 @@
+package com.storix.storix_api.domains.topicroom.domain;
+
+import com.storix.storix_api.global.model.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TopicRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "topic_room_id")
+    private Long id;
+
+    @Column(name = "topic_room_name", nullable = false)
+    private String topicRoomName;
+
+    @Column(name = "works_id", nullable = false)
+    private Long worksId;
+
+    @Column(name = "active_user_number", nullable = false)
+    private Integer activeUserNumber = 0;
+
+    @Builder
+    public TopicRoom(String topicRoomName, Long worksId) {
+        this.topicRoomName = topicRoomName;
+        this.worksId = worksId;
+    }
+
+    public void addParticipant() {
+        this.activeUserNumber++;
+    }
+    public void removeParticipant() {
+        this.activeUserNumber--;
+    }
+
+}
