5fbe12c8265540c47db62bf3a8a52a0c761a9aa0
caminobelllo <alstpgml0812@gmail.com>
2026-01-07 10:09:35 +0900

    Refactor: 오늘의 토픽룸 선정 기준 변경 (24시간)

--- src/main/java/com/storix/storix_api/domains/topicroom/repository/TopicRoomRepository.java
+++ src/main/java/com/storix/storix_api/domains/topicroom/repository/TopicRoomRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TopicRoomRepository extends JpaRepository<TopicRoom, Long> {
 
-    List<TopicRoom> findTop3ByOrderByActiveUserNumberDesc();
+    List<TopicRoom> findTop3ByCreatedAtAfterOrderByActiveUserNumberDesc(LocalDateTime threshold);
 
     @Query("SELECT t FROM TopicRoom t WHERE t.worksId IN :worksIds OR t.topicRoomName LIKE %:keyword%")
     Slice<TopicRoom> findBySearchCondition(@Param("worksIds") List<Long> worksIds, @Param("keyword") String keyword, Pageable pageable);
