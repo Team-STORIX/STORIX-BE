de9d4eef9ff13d3b364eab31f5754ee4f391675b
Immmii <leesa7118@gmail.com>
2026-01-12 03:09:18 +0900

    Refactor: S3 관련 controller 응답 포맷 변경

--- src/main/java/com/storix/storix_api/domains/image/controller/ImageController.java
+++ src/main/java/com/storix/storix_api/domains/image/controller/ImageController.java
@@ -2,15 +2,19 @@ package com.storix.storix_api.domains.image.controller;
 
 import com.storix.storix_api.domains.image.dto.FileUploadRequest;
 import com.storix.storix_api.domains.image.dto.PresignedUrlResponse;
+import com.storix.storix_api.domains.image.dto.ProfileImageUploadRequest;
 import com.storix.storix_api.domains.image.usecase.ImageUseCase;
 import com.storix.storix_api.domains.user.adaptor.AuthUserDetails;
 import com.storix.storix_api.global.apiPayload.CustomResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/image")
@@ -22,9 +26,9 @@ public class ImageController {
 
     @Operation(summary = "게시물 이미지 업로드", description = "S3 버킷에 게시물 이미지를 업로드할 수 있는 presignedUrl을 발급하는 api 입니다.   \n해당 url로 이미지를 업로드 한 후, url과 함께 반환된 objectKey는 게시물 등록 api로 보내주세요.")
     @PostMapping("/board")
-    public ResponseEntity<CustomResponse<PresignedUrlResponse>> getBoardImagePresignedUrl(
+    public ResponseEntity<CustomResponse<List<PresignedUrlResponse>>> getBoardImagePresignedUrl(
             @AuthenticationPrincipal AuthUserDetails authUserDetails,
-            @RequestBody FileUploadRequest req
+            @Valid @RequestBody FileUploadRequest req
     ) {
         return ResponseEntity.ok()
                 .body(imageUseCase.getBoardImagePresignedUrl(authUserDetails.getRole(), req));
@@ -33,7 +37,7 @@ public class ImageController {
     @Operation(summary = "프로필 이미지 업로드", description = "S3 버킷에 프로필 이미지를 업로드할 수 있는 presignedUrl을 발급하는 api 입니다.   \n해당 url로 이미지를 업로드 한 후, url과 함께 반환된 objectKey는 프로필 변경 api로 보내주세요.")
     @PostMapping("/profile")
     public ResponseEntity<CustomResponse<PresignedUrlResponse>> getProfileImagePresignedUrl(
-            @RequestBody FileUploadRequest req
+            @Valid @RequestBody ProfileImageUploadRequest req
     ) {
         return ResponseEntity.ok()
                 .body(imageUseCase.getProfileImagePresignedUrl(req));
