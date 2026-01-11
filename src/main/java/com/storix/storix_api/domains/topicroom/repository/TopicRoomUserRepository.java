443d20f4efdeed63af2488966db393f3e1d3767e
caminobelllo <alstpgml0812@gmail.com>
2026-01-12 00:52:50 +0900

    Refactor: 유저-토픽룸 fetch join 적용

--- src/main/java/com/storix/storix_api/domains/topicroom/repository/TopicRoomUserRepository.java
+++ src/main/java/com/storix/storix_api/domains/topicroom/repository/TopicRoomUserRepository.java
@@ -3,10 +3,13 @@ package com.storix.storix_api.domains.topicroom.repository;
 import com.storix.storix_api.domains.topicroom.domain.TopicRoomUser;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TopicRoomUserRepository extends JpaRepository<TopicRoomUser, Long> {
 
-    Slice<TopicRoomUser> findByUserId(Long userId, Pageable pageable);
+    @Query("select tru from TopicRoomUser tru join fetch tru.topicRoom where tru.userId = :userId")
+    Slice<TopicRoomUser> findByUserIdWithTopicRoom(@Param("userId") Long userId, Pageable pageable);
 
     long countByUserId(Long userId);
 
