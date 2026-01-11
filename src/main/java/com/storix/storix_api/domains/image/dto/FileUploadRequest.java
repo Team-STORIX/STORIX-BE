df076c2d03bbbe92e9932c54443d9b6a27e8cab9
Immmii <leesa7118@gmail.com>
2026-01-12 03:05:48 +0900

    Refactor: S3 요청 dto 분리 및 Validation 추가

--- src/main/java/com/storix/storix_api/domains/image/dto/FileUploadRequest.java
+++ src/main/java/com/storix/storix_api/domains/image/dto/FileUploadRequest.java
@@ -1,9 +1,19 @@
 package com.storix.storix_api.domains.image.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record FileUploadRequest(
-        List<FileInfo> files
+        @NotEmpty(message = "파일을 1개 이상 보내주세요.")
+        @Size(min = 1, max = 3, message = "프로필 이미지는 3개까지 업로드 가능합니다.")
+        List<@Valid FileInfo> files
 ) {
-    public record FileInfo(String contentType) {}
+    public record FileInfo(
+            @NotBlank(message = "contentType을 보내주세요.")
+            String contentType
+    ) {}
 }
