7d4894c9a881052048a531e774ed9584c42f342f
caminobelllo <alstpgml0812@gmail.com>
2026-01-13 00:27:32 +0900

    Refactor: 토픽룸 삭제 메서드 정의

--- src/main/java/com/storix/storix_api/domains/topicroom/application/port/RecordTopicRoomPort.java
+++ src/main/java/com/storix/storix_api/domains/topicroom/application/port/RecordTopicRoomPort.java
@@ -21,4 +21,6 @@ public interface RecordTopicRoomPort {
     void decrementActiveUserNumber(Long roomId);
 
     void updateLastChatTime(Long roomId, LocalDateTime now);
+
+    void deleteRoom(Long roomId);
 }
