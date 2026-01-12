0115067be38593b33be852d4a8f1b3e80e337bbf
caminobelllo <alstpgml0812@gmail.com>
2026-01-13 00:28:59 +0900

    Fix: N+1 해결을 위한 jpql 쿼리 작성

--- src/main/java/com/storix/storix_api/domains/topicroom/repository/TopicRoomRepository.java
+++ src/main/java/com/storix/storix_api/domains/topicroom/repository/TopicRoomRepository.java
@@ -13,13 +13,6 @@ import java.util.List;
 
 public interface TopicRoomRepository extends JpaRepository<TopicRoom, Long> {
 
-    List<TopicRoom> findTop3ByCreatedAtAfterOrderByActiveUserNumberDesc(LocalDateTime threshold);
-
-    @Query("SELECT t FROM TopicRoom t " +
-            "WHERE t.id NOT IN :excludeIds " +
-            "ORDER BY t.activeUserNumber DESC")
-    List<TopicRoom> findTopAllTimeExcluding(@Param("excludeIds") List<Long> excludeIds, Pageable pageable);
-
     @Query("""
         SELECT new com.storix.storix_api.domains.topicroom.dto.TopicRoomResponseDto(
             t.id, t.topicRoomName, w.worksType, w.worksName, w.thumbnailUrl, t.activeUserNumber, t.lastChatTime, false
@@ -29,6 +22,29 @@ public interface TopicRoomRepository extends JpaRepository<TopicRoom, Long> {
         WHERE w.id IN :worksIds OR t.topicRoomName LIKE %:keyword%
     """)
     Slice<TopicRoomResponseDto> findBySearchCondition(@Param("worksIds") List<Long> worksIds, @Param("keyword") String keyword, Pageable pageable);
+
+
+    @Query("""
+        SELECT new com.storix.storix_api.domains.topicroom.dto.TopicRoomResponseDto(
+            t.id, t.topicRoomName, w.worksType, w.worksName, w.thumbnailUrl, t.activeUserNumber, t.lastChatTime, false
+        )
+        FROM TopicRoom t
+        JOIN Works w ON t.worksId = w.id
+        WHERE t.createdAt > :threshold
+        ORDER BY t.activeUserNumber DESC
+    """)
+    List<TopicRoomResponseDto> findTop3TrendingWithWorks(@Param("threshold") LocalDateTime threshold);
+
+    @Query("""
+        SELECT new com.storix.storix_api.domains.topicroom.dto.TopicRoomResponseDto(
+            t.id, t.topicRoomName, w.worksType, w.worksName, w.thumbnailUrl, t.activeUserNumber, t.lastChatTime, false
+        )
+        FROM TopicRoom t
+        JOIN Works w ON t.worksId = w.id
+        WHERE t.id NOT IN :excludeIds
+        ORDER BY t.activeUserNumber DESC
+    """)
+    List<TopicRoomResponseDto> findTopAllTimeExcludingWithWorks(@Param("excludeIds") List<Long> excludeIds, Pageable pageable);
 
     @Modifying(clearAutomatically = true)
     @Query("UPDATE TopicRoom t SET t.activeUserNumber = t.activeUserNumber + 1 WHERE t.id = :id")
