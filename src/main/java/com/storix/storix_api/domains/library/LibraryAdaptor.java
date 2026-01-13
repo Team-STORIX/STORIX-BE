0d0d77741e287d4e6a559725e5c3e2bb6377a522
Immmii <leesa7118@gmail.com>
2026-01-13 17:12:11 +0900

    Feat: 서재 adaptor 리뷰개수 관련 메서드 구현

---
+++ src/main/java/com/storix/storix_api/domains/library/LibraryAdaptor.java
@@ -0,0 +1,21 @@
+package com.storix.storix_api.domains.library;
+
+import com.storix.storix_api.domains.library.repository.LibraryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LibraryAdaptor {
+
+    private final LibraryRepository libraryRepository;
+
+    public void incrementReviewCount(Long libraryUserId) {
+        libraryRepository.incrementReviewCount(libraryUserId);
+    }
+
+    public int decrementReviewCount(Long libraryUserId) {
+        return libraryRepository.decrementReviewCount(libraryUserId);
+    }
+
+}
