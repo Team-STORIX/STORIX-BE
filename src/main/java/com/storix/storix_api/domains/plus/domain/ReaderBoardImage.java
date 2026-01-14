e7a3d1e0dabd4943857cc75dd1659da2214c1d5a
Immmii <leesa7118@gmail.com>
2026-01-14 16:18:15 +0900

    Fix: [ReaderBoardImage] 공통 로직 추상화 추가

--- src/main/java/com/storix/storix_api/domains/plus/domain/ReaderBoardImage.java
+++ src/main/java/com/storix/storix_api/domains/plus/domain/ReaderBoardImage.java
@@ -5,13 +5,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Entity
 @Table(name = "reader_board_image")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ReaderBoardImage {
+public class ReaderBoardImage extends BoardImage {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reader_board_image_id")
@@ -20,13 +17,6 @@ public class ReaderBoardImage {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reader_board_id")
     private ReaderBoard readerBoard;
-
-    @Column(name = "image_object_key", nullable = false)
-    private String imageObjectKey;
-
-    @Column(name = "sort_order", nullable = false)
-    private int sortOrder;
-
 
     /** 생성자 로직 **/
     @Builder
