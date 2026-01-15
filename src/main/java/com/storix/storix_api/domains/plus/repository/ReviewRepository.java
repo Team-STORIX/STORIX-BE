c03128bc06a1e3265742c9c8e4c796ad46954f45
Immmii <leesa7118@gmail.com>
2026-01-15 23:59:52 +0900

    Feat: 서재->리뷰 repository 리뷰정보 조회 쿼리 구현

--- src/main/java/com/storix/storix_api/domains/plus/repository/ReviewRepository.java
+++ src/main/java/com/storix/storix_api/domains/plus/repository/ReviewRepository.java
@@ -1,12 +1,27 @@
 package com.storix.storix_api.domains.plus.repository;
 
 import com.storix.storix_api.domains.plus.domain.Review;
+import com.storix.storix_api.domains.plus.dto.ReviewedWorksIdAndRatingInfo;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     boolean existsByLibraryUserIdAndWorksId(Long libraryUserId, Long worksId);
+
+    @Query("SELECT new com.storix.storix_api.domains.plus.dto.ReviewedWorksIdAndRatingInfo(r.worksId, r.id, r.rating) " +
+            "FROM Review r " +
+            "WHERE r.libraryUserId = :userId")
+    Slice<ReviewedWorksIdAndRatingInfo> findWorksIdsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("SELECT new com.storix.storix_api.domains.plus.dto.ReviewedWorksIdAndRatingInfo(r.worksId, r.id, r.rating) " +
+            "FROM Review r " +
+            "WHERE r.libraryUserId = :userId")
+    List<ReviewedWorksIdAndRatingInfo> findAllWorksIdsByUserId(@Param("userId") Long userId);
 
 }
