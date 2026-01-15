326ea8671acd7f4e8d0052171a687466e39d9c08
Immmii <leesa7118@gmail.com>
2026-01-15 23:44:39 +0900

    Fix: 리뷰 domain 평점 필드 ENUM converter 구현

--- src/main/java/com/storix/storix_api/domains/plus/domain/Rating.java
+++ src/main/java/com/storix/storix_api/domains/plus/domain/Rating.java
@@ -12,18 +12,19 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public enum Rating {
 
-    ZERO_POINT_FIVE("0.5"),
-    ONE("1.0"),
-    ONE_POINT_FIVE("1.5"),
-    TWO("2.0"),
-    TWO_POINT_FIVE("2.5"),
-    THREE("3.0"),
-    THREE_POINT_FIVE("3.5"),
-    FOUR("4.0"),
-    FOUR_POINT_FIVE("4.5"),
-    FIVE("5.0");
+    ZERO_POINT_FIVE("0.5", 0.5),
+    ONE("1.0", 1.0),
+    ONE_POINT_FIVE("1.5", 1.5),
+    TWO("2.0", 2.0),
+    TWO_POINT_FIVE("2.5", 2.5),
+    THREE("3.0", 3.0),
+    THREE_POINT_FIVE("3.5", 3.5),
+    FOUR("4.0", 4.0),
+    FOUR_POINT_FIVE("4.5", 4.5),
+    FIVE("5.0", 5.0);
 
     private final String dbValue;
+    private final double ratingValue;
 
     @JsonCreator
     public static Rating from(String value) {
