21a9fb1166baefaba10335a6b91f7b8a974c994f
Immmii <leesa7118@gmail.com>
2026-01-18 23:04:14 +0900

    Chore: 사용하지 않는 import문 삭제

--- src/main/java/com/storix/storix_api/domains/profile/controller/ProfileController.java
+++ src/main/java/com/storix/storix_api/domains/profile/controller/ProfileController.java
@@ -15,7 +15,6 @@ import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
