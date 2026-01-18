1d6da9a92906b2d3d7645d872b680fc3fcf60685
Immmii <leesa7118@gmail.com>
2026-01-19 08:36:25 +0900

    Feat: 프로필 내가 쓴 게시글 관련 repository 쿼리 구현

--- src/main/java/com/storix/storix_api/domains/hashtag/repository/HashtagRepository.java
+++ src/main/java/com/storix/storix_api/domains/hashtag/repository/HashtagRepository.java
@@ -1,11 +1,23 @@
 package com.storix.storix_api.domains.hashtag.repository;
 
 import com.storix.storix_api.domains.hashtag.domain.Hashtag;
+import com.storix.storix_api.domains.hashtag.dto.HashtagInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 
     Optional<Hashtag> findByName(String name);
+
+    @Query("SELECT new com.storix.storix_api.domains.hashtag.dto.HashtagInfo(w.id, h.name) " +
+            "FROM Hashtag  h " +
+            "JOIN h.works w "+
+            "WHERE w.id IN :worksIds " +
+            "ORDER BY w.id, h.name")
+    List<HashtagInfo> findAllByWorksIds(@Param("worksIds") List<Long> worksIds);
+
 }
--- src/main/java/com/storix/storix_api/domains/plus/repository/ReaderBoardRepository.java
+++ src/main/java/com/storix/storix_api/domains/plus/repository/ReaderBoardRepository.java
@@ -1,7 +1,17 @@
 package com.storix.storix_api.domains.plus.repository;
 
 import com.storix.storix_api.domains.plus.domain.ReaderBoard;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReaderBoardRepository extends JpaRepository<ReaderBoard, Long> {
+
+    // 프로필 관련
+    @Query("SELECT rb " +
+            "FROM ReaderBoard rb " +
+            "WHERE rb.userId = :userId ")
+    Slice<ReaderBoard> findAllReaderBoardByUserId(Long userId, Pageable pageable);
+
 }
