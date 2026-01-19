52682c387a177962ab704679cf1e8420f1795bc9
caminobelllo <alstpgml0812@gmail.com>
2026-01-19 15:41:52 +0900

    Chore: StompHandler 주입

--- src/main/java/com/storix/storix_api/global/config/WebSocketConfig.java
+++ src/main/java/com/storix/storix_api/global/config/WebSocketConfig.java
@@ -1,29 +1,22 @@
 package com.storix.storix_api.global.config;
 
+import com.storix.storix_api.global.websocket.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-
-        registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("*");
-    }
+    private final StompHandler stompHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-
-        // 메세지 구독 요청 url (메세지 받을 때)
         registry.enableSimpleBroker("/sub");
-
-        // 메세지 발행 요청 url (메세지 보낼 때)
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
@@ -31,3 +24,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*");
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
+}
