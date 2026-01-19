371d139a3e34cec98cc60119507bc3a26cef1414
Immmii <leesa7118@gmail.com>
2026-01-20 07:35:16 +0900

    Refactor: 피드 관련 성공 응답 code 추가

--- src/main/java/com/storix/storix_api/domains/feed/usecase/FeedKebabUseCase.java
+++ src/main/java/com/storix/storix_api/domains/feed/usecase/FeedKebabUseCase.java
@@ -16,18 +16,18 @@ public class FeedKebabUseCase {
     // 내 게시물 삭제
     public CustomResponse<Void> deleteOwnBoard(Long userId, Long readerBoardId) {
         feedKebabService.deleteReaderBoard(userId, readerBoardId);
-        return CustomResponse.onSuccess(SuccessCode.SUCCESS);
+        return CustomResponse.onSuccess(SuccessCode.FEED_READER_BOARD_DELETE_SUCCESS);
     }
 
     // 게시물 신고
     public CustomResponse<Void> reportFeed(Long userId, Long boardId, FeedReportRequest req) {
         feedKebabService.reportFeed(userId, boardId, req);
-        return CustomResponse.onSuccess(SuccessCode.SUCCESS);
+        return CustomResponse.onSuccess(SuccessCode.FEED_READER_BOARD_REPORT_SUCCESS);
     }
 
     // 내 댓글 삭제
     public CustomResponse<Void> deleteOwnReply(Long userId, Long boardId, Long replyId) {
         feedKebabService.deleteReaderBoardReply(userId, boardId, replyId);
-        return CustomResponse.onSuccess(SuccessCode.SUCCESS);
+        return CustomResponse.onSuccess(SuccessCode.FEED_READER_BOARD_REPLY_DELETE_SUCCESS);
     }
 }
