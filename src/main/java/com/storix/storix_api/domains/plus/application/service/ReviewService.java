78f0ed2a1698025ddc8596dd9aae809f1c1da169
Immmii <leesa7118@gmail.com>
2026-01-19 17:54:34 +0900

    Feat: 리뷰 관련 기본 service 로직 구현

--- src/main/java/com/storix/storix_api/domains/plus/application/service/ReviewService.java
+++ src/main/java/com/storix/storix_api/domains/plus/application/service/ReviewService.java
@@ -6,16 +6,34 @@ import com.storix.storix_api.domains.plus.controller.dto.ReaderReviewRedirectRes
 import com.storix.storix_api.domains.plus.controller.dto.ReaderReviewUploadRequest;
 import com.storix.storix_api.domains.plus.domain.Review;
 import com.storix.storix_api.domains.plus.dto.CreateReviewCommand;
+import com.storix.storix_api.domains.plus.dto.ReviewInfo;
+import com.storix.storix_api.domains.plus.dto.SliceReviewInfo;
+import com.storix.storix_api.domains.review.dto.DetailedReviewInfoWithProfile;
+import com.storix.storix_api.domains.review.dto.SliceReviewInfoWithProfile;
+import com.storix.storix_api.domains.review.dto.StandardReviewInfo;
+import com.storix.storix_api.domains.review.dto.StandardSliceReviewInfo;
+import com.storix.storix_api.domains.user.adaptor.UserAdaptor;
+import com.storix.storix_api.domains.user.dto.StandardProfileInfo;
 import com.storix.storix_api.domains.works.application.helper.AdultWorksHelper;
 import com.storix.storix_api.domains.works.application.port.LoadWorksPort;
+import com.storix.storix_api.domains.works.dto.StandardWorksInfo;
+import com.storix.storix_api.domains.works.dto.WorksInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
 
+    private final UserAdaptor userAdaptor;
     private final ReviewAdaptor reviewAdaptor;
     private final LibraryAdaptor libraryAdaptor;
 
@@ -23,6 +41,7 @@ public class ReviewService {
 
     private final AdultWorksHelper adultWorksHelper;
 
+    // 플러스탭
     @Transactional
     public ReaderReviewRedirectResponse createReview(Long userId, ReaderReviewUploadRequest req) {
 
@@ -56,4 +75,68 @@ public class ReviewService {
         // 리뷰 중복 확인
         reviewAdaptor.existsByUserAndWorks(userId, worksId);
     }
+
+    // 작품 상세탭
+    @Transactional
+    public SliceReviewInfo findMyReview(Long userId, Long worksId) {
+
+        boolean isMyReviewExist = reviewAdaptor.isMyReviewExist(userId, worksId);
+
+        if (isMyReviewExist) {
+            return reviewAdaptor.getMyReviewInfo(userId, worksId);
+        } else {
+            return null;
+        }
+
+    }
+
+    @Transactional(readOnly = true)
+    public Slice<SliceReviewInfoWithProfile> findAllReviewWithoutMine(Long userId, Long worksId, Pageable pageable) {
+
+        // 1) 다른 사람의 리뷰 정보
+        Slice<SliceReviewInfo> reviews = reviewAdaptor.getOtherReviewInfo(userId, worksId, pageable);
+
+        if (reviews.isEmpty()) {
+            return new SliceImpl<>(List.of(), pageable, reviews.hasNext());
+        }
+
+        // 유저 id 리스트
+        List<Long> userIds = reviews.getContent().stream()
+                .map(SliceReviewInfo::userId)
+                .filter(Objects::nonNull)
+                .distinct()
+                .toList();
+
+        // 2) 프로필 정보
+        Map<Long, StandardProfileInfo> profileMap =
+                userAdaptor.findStandardProfileInfoByUserIds(userIds);
+
+        return reviews.map(review -> {
+            StandardProfileInfo profile = profileMap.get(review.userId());
+            return SliceReviewInfoWithProfile.of(StandardSliceReviewInfo.from(review), profile);
+        });
+    }
+
+    @Transactional(readOnly = true)
+    public DetailedReviewInfoWithProfile findReviewDetail(Long reviewId) {
+
+        // 1) 리뷰 정보
+        ReviewInfo reviewInfo = reviewAdaptor.findReviewById(reviewId);
+        StandardReviewInfo review = StandardReviewInfo.fromReviewInfo(reviewInfo);
+
+        // 2) 프로필 정보
+        Long userId = reviewInfo.reviewerId();
+
+        StandardProfileInfo profile =
+                userAdaptor.findStandardProfileInfoByUserId(userId);
+
+        // 3) 작품 정보
+        Long worksId = reviewInfo.worksId();
+
+        WorksInfo worksInfo = loadWorksPort.findWorksInfoById(worksId);
+        StandardWorksInfo works = StandardWorksInfo.from(worksInfo);
+
+        return DetailedReviewInfoWithProfile.of(profile, works, review);
+    }
+
 }
