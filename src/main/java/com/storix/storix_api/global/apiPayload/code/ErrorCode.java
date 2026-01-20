2718d0569d1fe3c2927743735f72136673c96f76
caminobelllo <alstpgml0812@gmail.com>
2026-01-20 09:28:08 +0900

    Fix: 토픽룸 유저 에러 코드 정의

--- src/main/java/com/storix/storix_api/global/apiPayload/code/ErrorCode.java
+++ src/main/java/com/storix/storix_api/global/apiPayload/code/ErrorCode.java
@@ -89,6 +89,7 @@ public enum ErrorCode {
     ALREADY_JOINED_ROOM(HttpStatus.CONFLICT, "TOPIC_ROOM_ERROR_005", "이미 참여 중인 토픽룸입니다."),
     SELF_REPORT_ERROR(HttpStatus.BAD_REQUEST, "TOPIC_ROOM_ERROR_006", "자기 자신은 신고할 수 없습니다."),
     TOPIC_ROOM_ALREADY_EXISTS(HttpStatus.CONFLICT, "TOPIC_ROOM_ERROR_007", "이미 해당 작품에 대한 토픽룸이 존재합니다."),
+    TOPIC_ROOM_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "TOPIC_ROOM_ERROR_008", "해당 토픽룸에 참여하지 않은 유저입니다."),
 
     // Works error
     WORKS_NOT_FOUND(HttpStatus.NOT_FOUND, "WORKS_ERROR_001", "해당 작품을 찾을 수 없습니다."),
