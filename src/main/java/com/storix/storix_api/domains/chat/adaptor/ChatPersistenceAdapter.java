b3f05b235371a8eb636ec174ee0662b5702df717
caminobelllo <alstpgml0812@gmail.com>
2026-01-20 11:58:23 +0900

    Fix: 반환 타입 수정

--- src/main/java/com/storix/storix_api/domains/chat/adaptor/ChatPersistenceAdapter.java
+++ src/main/java/com/storix/storix_api/domains/chat/adaptor/ChatPersistenceAdapter.java
@@ -3,13 +3,12 @@ package com.storix.storix_api.domains.chat.adaptor;
 import com.storix.storix_api.domains.chat.application.port.LoadChatPort;
 import com.storix.storix_api.domains.chat.application.port.RecordChatPort;
 import com.storix.storix_api.domains.chat.domain.ChatMessage;
+import com.storix.storix_api.domains.chat.dto.ChatMessageResponseDto;
 import com.storix.storix_api.domains.chat.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -18,7 +17,7 @@ public class ChatPersistenceAdapter implements RecordChatPort, LoadChatPort {
     private final ChatRepository chatRepository;
 
     @Override
-    public Slice<ChatMessage> loadMessages(Long roomId,  Pageable pageable) {
+    public Slice<ChatMessageResponseDto> loadMessages(Long roomId, Pageable pageable) {
         return chatRepository.findAllByRoomIdOrderByCreatedAtDesc(roomId, pageable);
     }
 
--- src/main/java/com/storix/storix_api/domains/chat/application/port/LoadChatPort.java
+++ src/main/java/com/storix/storix_api/domains/chat/application/port/LoadChatPort.java
@@ -1,9 +1,10 @@
 package com.storix.storix_api.domains.chat.application.port;
 
 import com.storix.storix_api.domains.chat.domain.ChatMessage;
+import com.storix.storix_api.domains.chat.dto.ChatMessageResponseDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface LoadChatPort {
-    Slice<ChatMessage> loadMessages(Long roomId, Pageable pageable);
+    Slice<ChatMessageResponseDto> loadMessages(Long roomId, Pageable pageable);
 }
