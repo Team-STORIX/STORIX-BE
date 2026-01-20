b359621007a8ac42f302ae1f26ae854f185ccd9d
Immmii <leesa7118@gmail.com>
2026-01-20 16:29:22 +0900

    Feat: 피드 댓글 신고 관련 repository/adaptor 구현

--- src/main/java/com/storix/storix_api/domains/feed/adaptor/FeedReportAdaptor.java
+++ src/main/java/com/storix/storix_api/domains/feed/adaptor/FeedReportAdaptor.java
@@ -1,8 +1,11 @@
 package com.storix.storix_api.domains.feed.adaptor;
 
+import com.storix.storix_api.domains.feed.domain.FeedReplyReport;
 import com.storix.storix_api.domains.feed.domain.FeedReport;
 import com.storix.storix_api.domains.feed.dto.CreateFeedReportCommand;
+import com.storix.storix_api.domains.feed.repository.FeedReplyReportRepository;
 import com.storix.storix_api.domains.feed.repository.FeedReportRepository;
+import com.storix.storix_api.global.apiPayload.exception.feed.DuplicateFeedReplyReportException;
 import com.storix.storix_api.global.apiPayload.exception.feed.DuplicateFeedReportException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class FeedReportAdaptor {
 
     private final FeedReportRepository feedReportRepository;
+    private final FeedReplyReportRepository feedReplyReportRepository;
 
     public void saveReport(CreateFeedReportCommand cmd) {
         try {
@@ -20,6 +24,15 @@ public class FeedReportAdaptor {
             feedReportRepository.save(feedReport);
         } catch (DataIntegrityViolationException e) {
             throw DuplicateFeedReportException.EXCEPTION;
+        }
+    }
+
+    public void saveReplyReport(CreateFeedReportCommand cmd) {
+        try {
+            FeedReplyReport feedReplyReport = cmd.toReplyEntity();
+            feedReplyReportRepository.save(feedReplyReport);
+        } catch (DataIntegrityViolationException e) {
+            throw DuplicateFeedReplyReportException.EXCEPTION;
         }
     }
 
