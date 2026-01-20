f886bc655731ff10eb38f34ec638fcf5e614ebae
caminobelllo <alstpgml0812@gmail.com>
2026-01-20 09:27:44 +0900

    Fix: 특정 유저의 참여 여부 확인 메서드 정의

--- src/main/java/com/storix/storix_api/domains/topicroom/application/port/LoadTopicRoomUserPort.java
+++ src/main/java/com/storix/storix_api/domains/topicroom/application/port/LoadTopicRoomUserPort.java
@@ -6,4 +6,7 @@ import java.util.Set;
 public interface LoadTopicRoomUserPort {
     // 특정 유저의 전체 토픽룸 입장 정보
     Set<Long> loadJoinedRoomIds(Long userId, List<Long> roomIds);
+
+    // 특정 유저가 특정 방에 참여 중인지 확인
+    boolean existsByUserIdAndRoomId(Long userId, Long roomId);
 }
