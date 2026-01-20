a0d2f66c7bc20318db20c0f0efdb903e07b572bc
Immmii <leesa7118@gmail.com>
2026-01-21 03:59:52 +0900

    Chore: 사용하지 않는 import문 삭제

--- src/main/java/com/storix/storix_api/domains/onboarding/controller/OnboardingWorksController.java
+++ src/main/java/com/storix/storix_api/domains/onboarding/controller/OnboardingWorksController.java
@@ -7,14 +7,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Validated
 @RestController
 @RequestMapping("/api/v1/onboarding")
 @RequiredArgsConstructor
