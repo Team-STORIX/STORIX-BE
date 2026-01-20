0bf8bd8145a20607da8cd2ef03dd55de02e75042
caminobelllo <alstpgml0812@gmail.com>
2026-01-20 09:27:29 +0900

    Fix: 토픽룸 검증 로직 추가

--- src/main/java/com/storix/storix_api/domains/chat/service/ChatService.java
+++ src/main/java/com/storix/storix_api/domains/chat/service/ChatService.java
@@ -6,10 +6,12 @@ import com.storix.storix_api.domains.chat.domain.ChatMessage;
 import com.storix.storix_api.domains.chat.dto.ChatMessageRequestDto;
 import com.storix.storix_api.domains.chat.dto.ChatMessageResponseDto;
 import com.storix.storix_api.domains.topicroom.application.port.LoadTopicRoomPort;
+import com.storix.storix_api.domains.topicroom.application.port.LoadTopicRoomUserPort;
 import com.storix.storix_api.domains.topicroom.application.port.UpdateTopicRoomPort;
 import com.storix.storix_api.domains.user.application.port.LoadUserPort;
 import com.storix.storix_api.domains.user.domain.User;
 import com.storix.storix_api.global.apiPayload.exception.topicRoom.UnknownTopicRoomException;
+import com.storix.storix_api.global.apiPayload.exception.topicRoom.UnknownTopicRoomUserException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -24,13 +26,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChatService implements ChatUseCase {
 
-    private final RecordChatPort recordChatPort;
     private final PublishChatPort publishChatPort;
     private final LoadUserPort loadUserPort;
     private final LoadTopicRoomPort loadTopicRoomPort;
-    private final UpdateTopicRoomPort updateTopicRoomPort;
     private final LoadChatPort loadChatPort;
     private final ChatAsyncService chatAsyncService;
+    private final LoadTopicRoomUserPort loadTopicRoomUserPort;
 
     @Override
     @Transactional
@@ -38,6 +39,16 @@ public class ChatService implements ChatUseCase {
 
 
         log.info(">>>> [ChatService] 메시지 전송 시도 - UserID: {}, RoomID: {}", userId, request.roomId());
+
+        // 토픽룸 존재 여부 검증
+        if (!loadTopicRoomPort.existsById(request.roomId())) {
+            throw UnknownTopicRoomException.EXCEPTION;
+        }
+
+        // 해당 토픽룸에 참여 중인 유저인지 검증
+        if (!loadTopicRoomUserPort.existsByUserIdAndRoomId(userId, request.roomId())) {
+            throw UnknownTopicRoomUserException.EXCEPTION;
+        }
 
         User user = loadUserPort.findById(userId);
         String nickname = user.getNickName();
