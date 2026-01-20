8ee21e4ab86a7fb8c5ea9beb959624116cefc366
caminobelllo <alstpgml0812@gmail.com>
2026-01-20 16:24:05 +0900

    Refactor: 메시지 발행 시 예외 처리

--- src/main/java/com/storix/storix_api/domains/chat/adaptor/RedisChatAdapter.java
+++ src/main/java/com/storix/storix_api/domains/chat/adaptor/RedisChatAdapter.java
@@ -2,10 +2,15 @@ package com.storix.storix_api.domains.chat.adaptor;
 
 import com.storix.storix_api.domains.chat.application.port.PublishChatPort;
 import com.storix.storix_api.domains.chat.dto.ChatMessageResponseDto;
+import com.storix.storix_api.global.apiPayload.exception.topicRoom.ChatConnectionFailureException;
+import com.storix.storix_api.global.apiPayload.exception.topicRoom.MessageDeliveryException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class RedisChatAdapter implements PublishChatPort {
 
@@ -18,7 +23,21 @@ public class RedisChatAdapter implements PublishChatPort {
     @Override
     public void publish(ChatMessageResponseDto response) {
 
-        // "room:1" 형식으로 메시지 전송
-        jsonRedisTemplate.convertAndSend("room:" + response.roomId(), response);
+        try {
+            // "room:1" 형식으로 메시지 전송
+            jsonRedisTemplate.convertAndSend("room:" + response.roomId(), response);
+        } catch (RedisConnectionFailureException e) {
+
+            // Redis 연결 실패
+            log.error(">>>> [Redis Error] Redis 연결 실패로 메시지 전송 불가 - RoomId: {}, Msg: {}",
+                    response.roomId(), response.message(), e);
+
+            throw ChatConnectionFailureException.EXCEPTION;
+        } catch (Exception e) {
+
+            // 기타 오류
+            log.error(">>>> [Redis Error] 메시지 발행 중 기타 오류 발생", e);
+            throw MessageDeliveryException.EXCEPTION;
+        }
     }
 }
