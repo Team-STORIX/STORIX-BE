b7574ccfa76313d7187499b36b7218fb277fad76
Immmii <leesa7118@gmail.com>
2026-01-21 02:52:28 +0900

    Feat: 온보딩 작품 리스트 usecase 구현

---
+++ src/main/java/com/storix/storix_api/domains/onboarding/usecase/OnboardingWorksUseCase.java
@@ -0,0 +1,23 @@
+package com.storix.storix_api.domains.onboarding.usecase;
+
+import com.storix.storix_api.UseCase;
+import com.storix.storix_api.domains.onboarding.adaptor.OnboardingWorksAdaptor;
+import com.storix.storix_api.domains.onboarding.domain.OnboardingWorks;
+import com.storix.storix_api.global.apiPayload.CustomResponse;
+import com.storix.storix_api.global.apiPayload.code.SuccessCode;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@UseCase
+@RequiredArgsConstructor
+public class OnboardingWorksUseCase {
+
+    private final OnboardingWorksAdaptor onboardingWorksAdaptor;
+
+    public CustomResponse<List<OnboardingWorks>> findAllOnboardingWorks() {
+        List<OnboardingWorks> result = onboardingWorksAdaptor.findAllOnboardingWorksList();
+        return CustomResponse.onSuccess(SuccessCode.SUCCESS, result);
+    }
+
+}
