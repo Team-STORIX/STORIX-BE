21fed8cad72c0b9da3052dfaa69fdbc314cd32ae
Immmii <leesa7118@gmail.com>
2026-01-22 17:02:50 +0900

    Feat: 프로필[취향 분석] 별점 분포 관련 usecase 구현

--- src/main/java/com/storix/storix_api/domains/profile/application/usecase/ProfileFavoriteUseCase.java
+++ src/main/java/com/storix/storix_api/domains/profile/application/usecase/ProfileFavoriteUseCase.java
@@ -3,6 +3,7 @@ package com.storix.storix_api.domains.profile.application.usecase;
 import com.storix.storix_api.UseCase;
 import com.storix.storix_api.domains.profile.application.service.ProfileFavoriteService;
 import com.storix.storix_api.domains.profile.dto.FavoriteWorksWithReviewInfo;
+import com.storix.storix_api.domains.profile.dto.RatingCountResponse;
 import com.storix.storix_api.domains.user.dto.FavoriteArtistInfo;
 import com.storix.storix_api.domains.profile.dto.ProfileFavoriteArtistWrapperDto;
 import com.storix.storix_api.domains.profile.dto.ProfileFavoriteWorksWrapperDto;
@@ -12,12 +13,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
+import java.util.Map;
+
 @UseCase
 @RequiredArgsConstructor
 public class ProfileFavoriteUseCase {
 
     private final ProfileFavoriteService profileFavoriteService;
 
+    // 관심 작가 조회
     public CustomResponse<ProfileFavoriteArtistWrapperDto<FavoriteArtistInfo>> getFavoriteArtistList(Long userId, Pageable pageable) {
 
         // 관심 작가 등록수
@@ -31,6 +35,7 @@ public class ProfileFavoriteUseCase {
         return CustomResponse.onSuccess(SuccessCode.PROFILE_FAVORITE_ARTIST_LIST_LOAD_SUCCESS, result);
     }
 
+    // 관심 작품 조회
     public CustomResponse<ProfileFavoriteWorksWrapperDto<FavoriteWorksWithReviewInfo>> getFavoriteWorksList(Long userId, Pageable pageable) {
 
         // 관심 작품 등록수
@@ -42,5 +47,12 @@ public class ProfileFavoriteUseCase {
                 = new ProfileFavoriteWorksWrapperDto<>(totalFavoriteWorksCount, favoriteWorksInfos);
 
         return CustomResponse.onSuccess(SuccessCode.PROFILE_FAVORITE_WORKS_LIST_LOAD_SUCCESS, result);
+    }
+
+    // 리뷰 별점 분포 조회
+    public CustomResponse<RatingCountResponse> getRatingDistribution(Long userId) {
+
+        RatingCountResponse result = profileFavoriteService.findRatingDistributionByUserId(userId);
+        return CustomResponse.onSuccess(SuccessCode.SUCCESS, result);
     }
 }
