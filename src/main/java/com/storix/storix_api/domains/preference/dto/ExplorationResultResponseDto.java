2e52cc7c134a2924e77e4b8b7ba7702a6a713760
caminobelllo <alstpgml0812@gmail.com>
2026-01-24 09:12:28 +0900

    Feat: 취향 탐색 결과 조회용 dto

---
+++ src/main/java/com/storix/storix_api/domains/preference/dto/ExplorationResultResponseDto.java
@@ -0,0 +1,18 @@
+package com.storix.storix_api.domains.preference.dto;
+
+import com.storix.storix_api.domains.library.dto.LibraryWorksInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ExplorationResultResponseDto {
+
+    private List<LibraryWorksInfo> likedWorks;
+    private List<LibraryWorksInfo> dislikedWorks;
+    private List<GenreScoreInfo> genreScores;
+}
