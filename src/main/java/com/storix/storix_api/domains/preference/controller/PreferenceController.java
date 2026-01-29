d2217cc4bc9797ae03f043f31d815be976277ff4
caminobelllo <alstpgml0812@gmail.com>
2026-01-29 09:29:19 +0900

    Chore: swagger 설정 추가

--- src/main/java/com/storix/storix_api/domains/preference/controller/PreferenceController.java
+++ src/main/java/com/storix/storix_api/domains/preference/controller/PreferenceController.java
@@ -20,9 +20,13 @@ public class PreferenceController {
 
     private final ExplorationUseCase explorationUseCase;
 
+    // 탐색 목록 조회
+    @Operation(summary = "취향 탐색용 작품 목록", description = "취향 탐색 시 사용할 작품 리스트를 조회합니다. 유저가 이미 취향 분석을 한 작품은 조회되지 않도록 합니다. " +
+            "취향 탐색은 하루 한 번만 가능합니다. 이미 한 경우에 재요청 할 경우 빈 리스트를 반환합니다.")
     @GetMapping("/exploration")
     public CustomResponse<List<ExplorationWorksResponseDto>> getExploration(
             @AuthenticationPrincipal AuthUserDetails authUserDetails) {
+
         return CustomResponse.onSuccess(
                 SuccessCode.SUCCESS,
                 explorationUseCase.getExplorationWorks(authUserDetails.getUserId())
@@ -30,6 +34,8 @@ public class PreferenceController {
 
     }
 
+    // 탐색 제출 (좋아요/별로예요)
+    @Operation(summary = "취향 분석", description = "개별 작품에 대한 분석 요청을 진행합니다.")
     @PostMapping("/exploration")
     public CustomResponse<String> submitResponse(
             @AuthenticationPrincipal AuthUserDetails authUserDetails,
@@ -39,9 +45,12 @@ public class PreferenceController {
         return CustomResponse.onSuccess(SuccessCode.SUCCESS);
     }
 
+    // 결과 페이지 조회 (오늘 진행한 15개)
+    @Operation(summary = "취향 분석 결과", description = "취향 분석 결과에 대해 조회합니다.")
     @GetMapping("/results")
     public CustomResponse<ExplorationResultResponseDto> getResults(
-            @AuthenticationPrincipal AuthUserDetails authUserDetails) {
+            @AuthenticationPrincipal AuthUserDetails authUserDetails
+    ) {
         return CustomResponse.onSuccess(SuccessCode.SUCCESS,
                 explorationUseCase.getExplorationResults(authUserDetails.getUserId()));
     }
