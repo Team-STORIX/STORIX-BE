5ee2b626fdaa781c05f959b91aa1067660ad3a6f
caminobelllo <alstpgml0812@gmail.com>
2026-01-29 09:27:35 +0900

    Feat: 배치 스케줄러 구현

---
+++ src/main/java/com/storix/storix_api/domains/preference/service/ExplorationBatchScheduler.java
@@ -0,0 +1,51 @@
+package com.storix.storix_api.domains.preference.service;
+
+import com.storix.storix_api.domains.preference.application.helper.ExplorationCacheHelper;
+import com.storix.storix_api.domains.preference.domain.PreferenceExploration;
+import com.storix.storix_api.domains.preference.dto.PendingSwipeDto;
+import com.storix.storix_api.domains.preference.repository.ExplorationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ExplorationBatchScheduler {
+
+    private final ExplorationCacheHelper cacheHelper;
+    private final ExplorationRepository explorationRepository;
+
+    // 5분마다 실행
+    @Scheduled(fixedDelay = 300000)
+    @Transactional
+    public void flushExplorationDataToDb() {
+
+        int batchSize = 100;
+
+        List<PendingSwipeDto> batch = cacheHelper.popBatchFromGlobalQueue(batchSize);
+
+        if (batch.isEmpty()) {
+            return;
+        }
+
+        List<PreferenceExploration> entities = batch.stream()
+                .map(dto -> PreferenceExploration.builder()
+                        .userId(dto.userId())
+                        .worksId(dto.worksId())
+                        .isLiked(dto.isLiked())
+                        .build())
+                .toList();
+
+        try {
+            explorationRepository.saveAll(entities);
+            log.info("Flushed {} exploration records to DB.", entities.size());
+        } catch (Exception e) {
+            log.error("Failed to save exploration batch to DB", e);
+        }
+    }
+}
